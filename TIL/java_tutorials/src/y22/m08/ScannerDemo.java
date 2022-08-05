baae05ef4b7acc18dead79e8067f3c9d10777552
EyEmilyKim surf <myue4555@gmail.com>
2022-08-06 00:57:03 +0900

    [INITIAL] 생활코딩 - 입력과 출력 _ 입력

---
+++ TIL/java_tutorials/src/y22/m08/ScannerDemo.java
@@ -0,0 +1,14 @@
+package y22.m08;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		System.out.println(i*1000);
+		sc.close();
+	}
+
+}
