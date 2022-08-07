982ebedc877087bb25c2a5b1d3f5ce1061db0be8
EyEmilyKim surf <myue4555@gmail.com>
2022-08-07 15:21:24 +0900

    [INITIAL] 생활코딩 - mul 메서드 확장  from Calculator0 (상속)

---
+++ TIL/java_tutorials/src/y22/m08/CalculatorDemo2.java
@@ -0,0 +1,14 @@
+package y22.m08;
+
+public class CalculatorDemo2 {
+
+	public static void main(String[] args) {
+		MulableCalculator0 c1 = new MulableCalculator0();
+		c1.setOperands(10, 20);
+		c1.sum();
+		c1.avg();
+		c1.mul();
+
+	}
+
+}
---
+++ TIL/java_tutorials/src/y22/m08/MulableCalculator0.java
@@ -0,0 +1,9 @@
+package y22.m08;
+
+public class MulableCalculator0 extends Calculator0 {
+	
+	public void mul() {
+		System.out.println(this.left * this.right);
+	}
+
+}
