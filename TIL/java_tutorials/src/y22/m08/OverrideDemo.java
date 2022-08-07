9810835be9a77cef614498b8523275d7c1887aa4
EyEmilyKim surf <myue4555@gmail.com>
2022-08-07 16:13:09 +0900

    [INITIAL] 생활코딩 - sum()재정의 (overriding)

---
+++ TIL/java_tutorials/src/y22/m08/OverrideDemo.java
@@ -0,0 +1,13 @@
+package y22.m08;
+
+public class OverrideDemo {
+
+	public static void main(String[] args) {
+		SubableCalculator c1 = new SubableCalculator(10,20);
+		c1.sub();
+		c1.sum();
+		c1.avg();
+
+	}
+
+}
