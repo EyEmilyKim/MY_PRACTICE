9c93c4a6b32ba4f89d26b2e1a30e138c7122e5a7
EyEmilyKim surf <myue4555@gmail.com>
2022-08-07 15:22:25 +0900

    [INITIAL] 생활코딩 - div 메서드 확장  from MulableCalculator0 from Calculator0 (상속)

---
+++ TIL/java_tutorials/src/y22/m08/CalculatorDemo3.java
@@ -0,0 +1,14 @@
+package y22.m08;
+
+public class CalculatorDemo3 {
+
+	public static void main(String[] args) {
+		
+		DivableCalculator0 c1 = new DivableCalculator0();
+		c1.setOperands(10, 20);
+		c1.sum();
+		c1.avg();
+		c1.mul();
+		c1.div();
+	}
+}
---
+++ TIL/java_tutorials/src/y22/m08/DivableCalculator0.java
@@ -0,0 +1,7 @@
+package y22.m08;
+
+public class DivableCalculator0 extends MulableCalculator0 {
+	public void div() {
+		System.out.println(this.left / this.right);
+	}
+}
