c1db8e8f570a66e6b8989cacc256065e19c9930e
EyEmilyKim Kosea1 <myue4555@gmail.com>
2022-08-09 12:56:14 +0900

    [UPDATE] DivideException 처리에 System.out.println(e.getMessage()); 추가

--- TIL/java_tutorials/src/y22/m08/exception/CustumExceptionDemo.java
+++ TIL/java_tutorials/src/y22/m08/exception/CustumExceptionDemo.java
@@ -34,49 +34,51 @@ class CalculatorE2{
 	
 		//2.Exception - checked
 		//2-1. try,catch 처리
-	public void divide() {
-		try {
-			if(this.right==0) {
-				throw new DivideException("0으로 나눌 수 없습니다.");
-			}
-			System.out.println(this.left / this.right);
-			System.out.println("\nDivide End");
-		} catch (DivideException e) {
-			e.printStackTrace();
-		}
-		
-	}	
+//	public void divide() {
+//		try {
+//			if(this.right==0) {
+//				throw new DivideException("0으로 나눌 수 없습니다.");
+//			}
+//			System.out.println(this.left / this.right);
+//			System.out.println("\nDivide End");
+//		} catch (DivideException e) {
+//			System.out.println(e.getMessage());
+//			e.printStackTrace();
+//		}
+//		
+//	}	
 		
 		//2-2. throw 처리 -> divide()의 사용자에게 예외 처리 위임
-//	public void divide() throws DivideException {
-//		if(this.right==0) {
-//			throw new DivideException("0으로 나눌 수 없습니다.");
-//		}
-//		System.out.println(this.left / this.right);
-//		System.out.println("\nDivide End");
-//			
-//	}
+	public void divide() throws DivideException {
+		if(this.right==0) {
+			throw new DivideException("0으로 나눌 수 없습니다.");
+		}
+		System.out.println(this.left / this.right);
+		System.out.println("\nDivide End");
+			
+	}
 
 }
 
 public class CustumExceptionDemo {
 
 		//1, 2-1. 사용자의 예외처리 없음
-	public static void main(String[] args) {
-		CalculatorE2 c1 = new CalculatorE2();
-		c1.setOprands(10, 0);
-		c1.divide();
-	}
-	
-		//2-2. throw 처리 -> divide()의 사용자에게 예외 처리 위임
 //	public static void main(String[] args) {
 //		CalculatorE2 c1 = new CalculatorE2();
 //		c1.setOprands(10, 0);
-//		try {
-//			c1.divide();
-//		} catch (DivideException e) {
-//			e.printStackTrace();
-//		}
+//		c1.divide();
 //	}
+//	
+		//2-2. throw 처리 -> divide()의 사용자에게 예외 처리 위임
+	public static void main(String[] args) {
+		CalculatorE2 c1 = new CalculatorE2();
+		c1.setOprands(10, 0);
+		try {
+			c1.divide();
+		} catch (DivideException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
 }
