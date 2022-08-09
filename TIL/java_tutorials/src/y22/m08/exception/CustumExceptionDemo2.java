afd853d2d7f4e89afc178f0df56b5784b4a9d824
EyEmilyKim kosea3 <myue4555@gmail.com>
2022-08-09 13:30:04 +0900

    [INITIAL] 생활코딩 - 예외3. 만들기 (예외 정보에 단서 추가 on both of 1.try,catch / 2.throw)

--- TIL/java_tutorials/src/y22/m08/exception/CustumExceptionDemo2.java
+++ TIL/java_tutorials/src/y22/m08/exception/CustumExceptionDemo2.java
@@ -1,15 +1,17 @@
 package y22.m08.exception;
 
+	// 직접 만든 예외: DivideException클래스 생성
+	// ↓ 2.checked로 하기 위해 Exception클래스 상속. 
 class DivideException2 extends Exception {
 	
 	public int left; 
 	public int right;
 	
-	DivideException2(){
+	DivideException2(){//기본 생성자
 		super();
 	}
 	
-	//String 매개변수가 "3개" 있는 생성자
+	//매개변수가 "3개" 있는 생성자 - 예외 단서 추가!
 	DivideException2(String message, int left, int right){
 		super(message);
 		this.left = left;
@@ -26,52 +28,55 @@ class CalculatorE3{
 
 		//2.Exception - checked
 		//2-1. try,catch 처리
-//	public void divide() {
-//		try {
-//			if(this.right==0) {
-//				throw new DivideException2("0으로 나눌 수 없습니다.") ;
-//			}
-//			System.out.println(this.left / this.right);
-//			System.out.println("\nDivide End");
-//		} catch (DivideException2 e) {
-//			e.printStackTrace();
-//		}
-//		
-//	}	
-		
-		//2-2. throw 처리 -> divide()의 사용자에게 예외 처리 위임
-	public void divide() throws DivideException2 {
-		if(this.right==0) {
-			throw new DivideException2("0으로 나눌 수 없습니다.", this.left, this.right);
-		}
-		System.out.println(this.left / this.right);
-		System.out.println("\nDivide End");
-			
-	}
-
-}
-
-public class CustumExceptionDemo2 {
-
-		//1, 2-1. 사용자의 예외처리 없음
-//	public static void main(String[] args) {
-//		CalculatorE3 c1 = new CalculatorE3();
-//		c1.setOprands(10, 0);
-//		c1.divide();
-//	}
-	
-		//2-2. throw 처리 -> divide()의 사용자에게 예외 처리 위임
-	public static void main(String[] args) {
-		CalculatorE3 c1 = new CalculatorE3();
-		c1.setOprands(10, 0);
+	public void divide() {
 		try {
-			c1.divide();
+			if(this.right==0) {
+				throw new DivideException2("0으로 나눌 수 없습니다.", this.left, this.right) ;
+			}
+			System.out.println(this.left / this.right);
+			System.out.println("\nDivide End");
 		} catch (DivideException2 e) {
+			e.printStackTrace();
 			System.out.println(e.getMessage());
 			System.out.println("입력한 좌항 : "+e.left);
 			System.out.println("입력한 우항 : "+e.right);
 		}
+		
 	}
+		
+		//2-2. throw 처리 -> divide()의 사용자에게 예외 처리 위임
+//	public void divide() throws DivideException2 {
+//		if(this.right==0) {
+//			throw new DivideException2("0으로 나눌 수 없습니다.", this.left, this.right);
+//		}
+//		System.out.println(this.left / this.right);
+//		System.out.println("\nDivide End");
+//	}
+
+ }
+
+public class CustumExceptionDemo2 {
+
+		//1, 2-1. 사용자의 예외처리 없음
+	public static void main(String[] args) {
+		CalculatorE3 c1 = new CalculatorE3();
+		c1.setOprands(10, 0);
+		c1.divide();
+	}
+	
+		//2-2. throw 처리 -> divide()의 사용자에게 예외 처리 위임
+//	public static void main(String[] args) {
+//		CalculatorE3 c1 = new CalculatorE3();
+//		c1.setOprands(10, 0);
+//		try {
+//			c1.divide();
+//		} catch (DivideException2 e) {
+//			e.printStackTrace();
+//			System.out.println(e.getMessage());
+//			System.out.println("입력한 좌항 : "+e.left);
+//			System.out.println("입력한 우항 : "+e.right);
+//		}
+//	}
 
 }
 
