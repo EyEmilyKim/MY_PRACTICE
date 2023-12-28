8a8407f9b45df61be6072b3fe69ccf86329fa87a
EyEmilyKim surf <myue4555@gmail.com>
2023-12-28 15:35:38 +0900

    [UPDATE] 코알누 - 채팅앱1 / 3. 백-프론트 연결 테스트... connected, disconnected 확인

--- Nuna-ChatApp/utils/io.js
+++ Nuna-ChatApp/utils/io.js
@@ -2,5 +2,10 @@ module.exports = function (io){
     // io 관련 모든 일
     io.on("connection", async(socket)=>{
         console.log("client is connected : ", socket.id);
+        
+        
+        socket.on("disconnect", ()=>{
+            console.log("user is disconnected : ", socket.id);
+        })
     });
 };
