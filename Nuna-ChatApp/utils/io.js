5e02699c3356964afaad6593cbf47daa9d8498e6
EyEmilyKim surf <myue4555@gmail.com>
2023-12-29 14:52:24 +0900

    [UPDATE] 코알누 - 채팅앱2 / 4. 로그인... userName propt, 전달

--- Nuna-ChatApp/utils/io.js
+++ Nuna-ChatApp/utils/io.js
@@ -3,7 +3,13 @@ module.exports = function (io){
     io.on("connection", async(socket)=>{
         console.log("client is connected : ", socket.id);
         
-        
+        socket.on("login", async(userName, cb)=>{
+            console.log("backend userName : ", userName);
+
+            //User 정보를 저장
+            
+        })
+
         socket.on("disconnect", ()=>{
             console.log("user is disconnected : ", socket.id);
         })
--- Nuna-ChatApp_Client/src/App.js
+++ Nuna-ChatApp_Client/src/App.js
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
 import "./App.css";
 import socket from "./server";
 
 function App() {
+
+  useEffect(()=>{
+    askUserName();
+  }, []);
+  
+  const askUserName = ()=>{
+    const userName = prompt("당신의 이름을 입력하세요")
+    console.log("user name : ", userName);
+
+    socket.emit("login", userName, (res)=>{
+      console.log("Res : ", res);
+    })
+  }
   return (
     <div>
       <div className="App"></div>
