23ba17e56aa3b2fd484969de4c7b4d851c2e92ab
EyEmilyKim surf <myue4555@gmail.com>
2024-01-04 00:52:10 +0900

    [UPDATE] 코알누 - 채팅앱+ /  5. 홈 페이지 분리... LoginModal 확인

--- Nuna-ChatApp_Client/src/components/pages/HomePage/HomePage.jsx
+++ Nuna-ChatApp_Client/src/components/pages/HomePage/HomePage.jsx
@@ -1,22 +1,13 @@
-
-import { Button } from '@mui/base'; 
-import { useState } from 'react';
+import LoginModal from '../../LoginModal/LoginModal';
+import './HomePage.css';
 
 const HomePage = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleOpen = () => { setIsOpen(true); }
-    const handleClose = () => { setIsOpen(false); }
 
     return (
         <div className="home-body">
-            <h1>뇽챗 HomePage입니다</h1>
+            <h1 className='home-title'>뇽챗 HomePage입니다</h1>
 
-            <Button onClick={handleOpen}>
-                로그인 하기
-            </Button>
-
-        
+            <LoginModal className="login-area"></LoginModal>
 
         </div>
     )
