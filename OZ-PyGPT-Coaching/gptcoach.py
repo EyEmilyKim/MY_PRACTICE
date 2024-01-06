3ae2eb193d62a7a80b89bb85be0ed97602512def
EyEmilyKim surf <myue4555@gmail.com>
2024-01-06 19:23:35 +0900

    [INITIAL] 오즈코딩 - PyGPT 코칭앱 / 7. 유저에게 정보 입력받기

--- OZ-PyGPT-Coaching/gptcoach.py
+++ OZ-PyGPT-Coaching/gptcoach.py
@@ -8,15 +8,35 @@ client = OpenAI() # API 클라이언트 인스턴스 생성. API_KEY는 .env 읽
 model = "gpt-3.5-turbo" # chatGPT 모델 설정
 
 # 질의 내용
-query = """
-나의 MBTI : INFP
-상대방 MBTI : ISFP
-나의 성별/나이 : 여자/34
-상대방 성별/나이 : 남자/26
-관계 : 연인
-상황 : 만난지 1년된 상황
-질문 : 1주년 선물로 뭐가 좋을지 추천해줄래? 보통 이 나이대 여자는 남자친구에게 어떤 걸 선물해?
-"""
+# query = """
+# 나의 MBTI : INFP
+# 상대방 MBTI : ISFP
+# 나의 성별/나이 : 여자/34
+# 상대방 성별/나이 : 남자/26
+# 관계 : 연인
+# 상황 : 만난지 1년된 상황
+# 질문 : 1주년 선물로 뭐가 좋을지 추천해줄래? 보통 이 나이대 여자는 남자친구에게 어떤 걸 선물해?
+# """
+
+# 질의에 필요한 정보 입력 받기
+my_mbti = input("나의 MBTI : ").upper()
+your_mbti = input("상대방 MBTI : ").upper()
+my_gender_age = input("나의 성별/나이 : ")
+your_gender_age = input("상대방 성별/나이 : ")
+relation = input("관계 : ")
+situation = input("상황 : ")
+question = input("질문 : ")
+
+# 질의문 만들기
+query = f"""나의 MBTI : {my_mbti}
+        상대방 MBTI : {your_mbti}
+        나의 성별/나이 : {my_gender_age}
+        상대방 성별/나이 : {your_gender_age}
+        관계 : {relation}
+        상황 :  {situation}
+        질문 :  {question}
+        앞으로 내가 또 할 질문에 항상 이 정보와 상황을 참고해줘
+        """
 
 # 질의 요청 준비
 messages = [
